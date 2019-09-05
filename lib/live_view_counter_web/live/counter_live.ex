9c91ed81779b8e92246c392f68dc51dcecf3f7a8
Brett Sanders <brettrsanders@gmail.com>
2019-09-04 23:56:29 -0400

    Hero move only every 500ms

--- lib/live_view_counter_web/live/counter_live.ex
+++ lib/live_view_counter_web/live/counter_live.ex
@@ -4,6 +4,7 @@ defmodule LiveViewCounterWeb.CounterLive do
 
   @dungeon_width 25
   @dungeon_height 25
+  @hero_tick_speed 500
 
   def render(assigns) do
     CounterView.render("index.html", assigns)
@@ -11,12 +12,13 @@ defmodule LiveViewCounterWeb.CounterLive do
 
   def mount(_session, socket) do
     if connected?(socket) do
-      :timer.send_interval(1000, self(), :tick)
+      :timer.send_interval(@hero_tick_speed, self(), :tick)
     end
 
     defaults = %{
       count: 0,
-      hero_position: {0, 0}
+      hero_position: {0, 0},
+      hero_can_move: true
     }
 
     new_socket =
@@ -36,59 +38,96 @@ defmodule LiveViewCounterWeb.CounterLive do
 
   def handle_event("keydown", value, socket) do
     %{"code" => direction} = value
-    {x, y} = socket.assigns.hero_position
-    # time = socket.assigns.count
 
-    new_hero_position =
-      case direction do
-        "ArrowLeft" ->
-          IO.puts("Decrement X Now")
+    old_hero_position = socket.assigns.hero_position
+    hero_can_move = socket.assigns.hero_can_move
 
-          # Detect left boundary
-          if x - 1 >= 0 do
-            {x - 1, y}
-          else
-            {x, y}
-          end
+    new_hero_position = determine_new_hero_position(direction, old_hero_position)
+    hero_moved? = old_hero_position != new_hero_position
 
-        "ArrowRight" ->
-          IO.puts("Increment X Now")
+    hero_position =
+      case hero_can_move do
+        true ->
+          new_hero_position
 
-          if x < @dungeon_width do
-            {x + 1, y}
-          else
-            {x, y}
-          end
-
-        "ArrowUp" ->
-          IO.puts("Increment Y Now")
-
-          if(y < @dungeon_height) do
-            {x, y + 1}
-          else
-            {x, y}
-          end
-
-        "ArrowDown" ->
-          IO.puts("Decrement Y Now")
-
-          if y > 0 do
-            {x, y - 1}
-          else
-            {x, y}
-          end
+        false ->
+          old_hero_position
 
         _ ->
-          IO.puts("Ignore this")
-          {x, y}
+          old_hero_position
       end
 
-    new_socket = socket |> assign(hero_position: new_hero_position)
+    hero_can_move = if hero_moved?, do: false
+
+    new_socket =
+      socket
+      |> assign(
+        hero_position: hero_position,
+        hero_can_move: hero_can_move
+      )
 
     {:noreply, new_socket}
   end
 
   def handle_info(:tick, socket) do
-    {:noreply, update(socket, :count, &(&1 + 1))}
+    IO.puts("Tick")
+    IO.puts(:os.system_time(:millisecond))
+
+    new_socket =
+      socket
+      |> assign(
+        count: socket.assigns.count + 1,
+        hero_can_move: true
+      )
+
+    #  update(socket, :count, &(&1 + 1))
+    {:noreply, new_socket}
+  end
+
+  defp determine_new_hero_position(direction, old_hero_position) do
+    {x, y} = old_hero_position
+
+    case direction do
+      "ArrowLeft" ->
+        IO.puts("Decrement X Now")
+
+        # Detect left boundary
+        if x - 1 >= 0 do
+          {x - 1, y}
+        else
+          {x, y}
+        end
+
+      "ArrowRight" ->
+        IO.puts("Increment X Now")
+
+        if x < @dungeon_width do
+          {x + 1, y}
+        else
+          {x, y}
+        end
+
+      "ArrowUp" ->
+        IO.puts("Increment Y Now")
+
+        if(y < @dungeon_height) do
+          {x, y + 1}
+        else
+          {x, y}
+        end
+
+      "ArrowDown" ->
+        IO.puts("Decrement Y Now")
+
+        if y > 0 do
+          {x, y - 1}
+        else
+          {x, y}
+        end
+
+      _ ->
+        IO.puts("Ignore this")
+        {x, y}
+    end
   end
 end
